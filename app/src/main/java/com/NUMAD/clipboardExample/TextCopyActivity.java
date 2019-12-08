192187fc516f27cf73f85d714d2f9255c82589fd
bgavinash <bgavinash@gmail.com>
2019-12-08 15:36:27 -0500

    Copy Text working

--- app/src/main/java/com/NUMAD/clipboardExample/TextCopyActivity.java
+++ app/src/main/java/com/NUMAD/clipboardExample/TextCopyActivity.java
@@ -12,14 +12,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import static android.content.ClipDescription.MIMETYPE_TEXT_PLAIN;
 
 public class TextCopyActivity extends AppCompatActivity {
 
     ClipboardManager clipboard;
     String pasteText;
 
+    Button pasteButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +31,27 @@ public class TextCopyActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         clipboard = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        pasteButton = findViewById(R.id.pasteTextButton);
+        // If the clipboard doesn't contain data, disable the paste menu item.
+// If it does contain data, decide if you can handle the data.
+        changePasteButtonVisibility();
 
+    }
+
+    private void changePasteButtonVisibility(){
+        if (!(clipboard.hasPrimaryClip())) {
+
+            pasteButton.setEnabled(false);
+
+        } else if (!(clipboard.getPrimaryClipDescription().hasMimeType(MIMETYPE_TEXT_PLAIN))) {
+
+            // This disables the paste menu item, since the clipboard has data but it is not plain text
+            pasteButton.setEnabled(false);
+        } else {
+
+            // This enables the paste menu item, since the clipboard contains plain text.
+            pasteButton.setEnabled(true);
+        }
     }
 
     public void copyText(View view) {
@@ -39,13 +63,17 @@ public class TextCopyActivity extends AppCompatActivity {
         // Set the clipboard's primary clip.
         clipboard.setPrimaryClip(clip);
 
+        changePasteButtonVisibility();
+
     }
 
     public void pasteText(View view) {
         ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
-        pasteText = item.getText().toString();
-        TextView tv = view.getRootView().findViewById(R.id.destinationTextview);
-        tv.setText(pasteText);
+        if(item!= null) {
+            pasteText = item.getText().toString();
+            TextView tv = view.getRootView().findViewById(R.id.destinationTextview);
+            tv.setText(pasteText);
+        }
     }
 
 
